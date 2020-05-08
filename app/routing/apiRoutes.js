0c98dd5f2d7dd0b61f9a86d087c5e3e2dc62963a
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-05-08 12:54:07 -0500

    large file upload

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,50 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+var path = require("path");
+var friendData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+    
+    var newFriend = req.body;
+
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    friendData.push(newFriend);
+    res.json(true);
+
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    friendsData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
