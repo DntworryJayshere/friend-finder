1f96e3a73d6220e7c95a609fd4267a4778d696bf
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-08-22 14:15:10 -0500

    removal of comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,85 +4,48 @@
 // ===============================================================================
 
 var friendsArray = [
-    {
-        "name": "Beth",
-        "photo": "https://www.pexels.com/photo/women-s-white-and-black-button-up-collared-shirt-774909/",
-        "scores": [
-            "5",
-            "1",
-            "4",
-            "4",
-            "5"
-        ]
-    },
-    {
-        "name": "John",
-        "photo": "https://www.pexels.com/photo/adult-beard-boy-casual-220453/",
-        "scores": [
-            "4",
-            "2",
-            "5",
-            "1",
-            "3"
-        ]
-    },
-    {
-        "name": "Savannah",
-        "photo": "https://www.pexels.com/photo/adult-african-american-afro-black-female-1181519/",
-        "scores": [
-            "5",
-            "2",
-            "2",
-            "2",
-            "4"
-        ]
-    },
-    {
-        "name": "Ash",
-        "photo": "https://www.pexels.com/photo/photography-of-a-guy-wearing-green-shirt-1222271/",
-        "scores": [
-            "3",
-            "3",
-            "4",
-            "2",
-            "2"
-        ]
-    },
-    {
-        "name": "Frank",
-        "photo": "https://www.pexels.com/photo/man-wearing-black-zip-up-jacket-near-beach-smiling-at-the-photo-736716/",
-        "scores": [
-            "4",
-            "3",
-            "4",
-            "1",
-            "5"
-        ]
-    },
-    {
-        "name": "Liam",
-        "photo": "https://www.pexels.com/photo/man-in-red-crew-neck-sweatshirt-photography-941693/",
-        "scores": [
-            "4",
-            "4",
-            "2",
-            "3",
-            "2"
-        ]
-    },
-    {
-        "name": "Andre",
-        "photo": "https://www.pexels.com/photo/man-in-white-dress-shirt-and-black-blazer-standing-outside-building-832998/",
-        "scores": [
-            "3",
-            "3",
-            "3",
-            "3",
-            "3"
-        ]
-    }
-]
-
+  {
+    name: 'Beth',
+    photo:
+      'https://www.pexels.com/photo/women-s-white-and-black-button-up-collared-shirt-774909/',
+    scores: ['5', '1', '4', '4', '5'],
+  },
+  {
+    name: 'John',
+    photo: 'https://www.pexels.com/photo/adult-beard-boy-casual-220453/',
+    scores: ['4', '2', '5', '1', '3'],
+  },
+  {
+    name: 'Savannah',
+    photo:
+      'https://www.pexels.com/photo/adult-african-american-afro-black-female-1181519/',
+    scores: ['5', '2', '2', '2', '4'],
+  },
+  {
+    name: 'Ash',
+    photo:
+      'https://www.pexels.com/photo/photography-of-a-guy-wearing-green-shirt-1222271/',
+    scores: ['3', '3', '4', '2', '2'],
+  },
+  {
+    name: 'Frank',
+    photo:
+      'https://www.pexels.com/photo/man-wearing-black-zip-up-jacket-near-beach-smiling-at-the-photo-736716/',
+    scores: ['4', '3', '4', '1', '5'],
+  },
+  {
+    name: 'Liam',
+    photo:
+      'https://www.pexels.com/photo/man-in-red-crew-neck-sweatshirt-photography-941693/',
+    scores: ['4', '4', '2', '3', '2'],
+  },
+  {
+    name: 'Andre',
+    photo:
+      'https://www.pexels.com/photo/man-in-white-dress-shirt-and-black-blazer-standing-outside-building-832998/',
+    scores: ['3', '3', '3', '3', '3'],
+  },
+];
 
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
